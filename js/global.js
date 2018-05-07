249c5a02b0f812cb65600268ec62cc663fec4bd0
Daniela <daniela.vornic@gmail.com>
2018-05-07 18:09:55 +0300

    Add section 2 + scroll transitions

---
+++ js/global.js
@@ -0,0 +1,31 @@
+window.sr = ScrollReveal({
+    reset: true
+});
+sr.reveal('#nav-arrow', {
+    duration: 1500,
+});
+sr.reveal('nav ul', {
+    duration: 1500,
+});
+sr.reveal('#header-content div:nth-of-type(1)', {
+    duration: 1500,
+});
+sr.reveal('#header-content div:nth-of-type(2)', {
+    duration: 1500,
+});
+sr.reveal('#about img', {
+    origin: 'left',
+    duration: 1500
+});
+sr.reveal('#about div:nth-of-type(2)', {
+    origin: 'right',
+    duration: 1500
+    
+});
+
+new TypeIt('.typed', {
+    strings: 'This is my string!',
+    speed: 150,
+    cursor: false,
+    loop: true
+});
